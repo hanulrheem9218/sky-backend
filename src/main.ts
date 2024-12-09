97b27063994a3703ce2c4efe96147f80caee5a45
hanulrheem9218 <114366902+hanulrheem9218@users.noreply.github.com>
2024-12-09 22:45:14 +1300

    chore(vercel): update vercel

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,48 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://localhost:3000', 'http://localhost:3000'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Authroization',
-  });
-  await app.listen(process.env.PORT ?? 5000);
-  return app;
-}
-bootstrap();
+// Keep the app instance in memory for subsequent requests
+let app;
+export default async function handler(req, res) {
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: ['https://localhost:3000', 'http://localhost:3000'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+      allowedHeaders: 'Content-Type, Authroization',
+    });
+    const config = new DocumentBuilder()
+      .setTitle('EasyNotes API')
+      .setDescription('Documentation for EasyNotes API')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
 
-export function GET(request: Request) {
-  return bootstrap();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        // Require decorator for field to be present
+        whitelist: true,
+
+        // Use class-transformer
+        transform: true,
+
+        // Use validator and transformer in response
+        always: true,
+      }),
+    );
+
+    SwaggerModule.setup('api', app, document);
+
+    // This is important
+    await app.init();
+  }
+  const adapterHost = app.get(HttpAdapterHost);
+  const httpAdapter = adapterHost.httpAdapter;
+  const instance = httpAdapter.getInstance();
+
+  instance(req, res);
 }
