0f6777d776aa556e73f0dc230b2429a51153e2dc
hanulrheem9218 <114366902+hanulrheem9218@users.noreply.github.com>
2024-12-09 22:16:15 +1300

    chore(vercel): update vercel

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+export default async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://localhost:3000', 'http://localhost:3000'],
