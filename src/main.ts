07619bba4805418b4ec94db3631b16c77f484600
hanulrheem9218 <114366902+hanulrheem9218@users.noreply.github.com>
2024-12-09 23:57:18 +1300

    chore(vercel): update vercel

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors({
     origin: ['https://localhost:3000', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-    allowedHeaders: 'Content-Type, Authroization',
+    allowedHeaders: 'Content-Type, Authorization',
   });
-  await app.listen(process.env.PORT ?? 5000);
+  await app.init();
 }
+
+// Call the bootstrap function
 bootstrap();
+
+// Export the server to make it available to Vercel
+export default server;
