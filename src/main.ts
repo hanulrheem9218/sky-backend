71ea76c8d73e250100f03ea5535c7d740a7c0a5d
hanulrheem9218 <114366902+hanulrheem9218@users.noreply.github.com>
2024-12-09 15:45:17 +1300

    chore(cors): update policy

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: ['https://localhost:3000', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authroization',
+  });
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
